c4fa807dbfd66843bae018735df3df8c412299a1
parham bagherzadeh <parhamb23@gmail.com>
2020-12-11 01:08:29 +0330

    before use sequen...

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,16 +6,16 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 class Util{
-     static String basePath="C:\\Users\\bghpa\\Desktop\\New folder\\dblp-explorer";
-     static String filePath=basePath+"\\src\\main\\java\\dblp_papers_v11.txt";
-     static String resPath=basePath;
+
+     static String dbPath;
+     static String resPath;
     public static Boolean isContain(String keyword,String article){
         return article.contains(keyword);
     }
@@ -39,27 +39,48 @@ class Util{
 
 public class Main {
     public static void main(String[] args) throws IOException{
-        int n=5;
-        String keyword="parham";
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter key word:");
+        String keyword = scanner.nextLine();
+        System.out.println("Enter n :");
+        String nStr = scanner.nextLine();
+        int n = Integer. parseInt(nStr);
+
+        Util.dbPath="C:\\Users\\bghpa\\Desktop\\New folder\\dblp-explorer\\src\\main\\resources\\1.txt";
+        Util.resPath="C:\\Users\\bghpa\\Desktop\\New folder\\dblp-explorer\\src\\main\\resources\\";
 
 
         Supplier<Stream<String>> articlesS
                 = () -> {
             try {
-                return Files.lines(Path.of(Util.filePath));
+                return Files.lines(Path.of(Util.dbPath));
             } catch (IOException e) {
                 e.printStackTrace();
                 return Stream.of("");
             }
-        }; ;
-
-        Stream<String> tireStream=articlesS.get().filter(articleStr->Util.isContain(keyword,articleStr))
-                .map(s->{ return new JSONObject(s); }).map(ja->(String)ja.get("id"));
+        };
         System.out.println("tire 1");
+        List<JSONObject> tire1List=articlesS.get().filter(articleStr->Util.isContain(keyword,articleStr))
+                .map(s->{ return new JSONObject(s); }).collect(Collectors.toList());
 
-        List<Object> workList=tireStream.collect(Collectors.toList());
+        Supplier<Stream<JSONObject>> tire1S
+                = () -> {
+            return tire1List.stream();
+        };
+        FileWriter tire1Writer = new FileWriter(Util.resPath+"tire1.txt");
+        tire1S.get().forEach(j->{
+            try {
+                tire1Writer.write(j.toString());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        tire1Writer.close();
+        List<Object> workList=tire1S.get().map(ja->(String)ja.get("id")).collect(Collectors.toList());
 
         for (int i=2;i<n+1;i++) {
+            System.out.println(workList);
             System.out.print("tire");
             System.out.println(i);
 
@@ -86,11 +107,12 @@ public class Main {
             FileWriter finalMyWriter = myWriter;
             objsS.get().forEach(j -> {
                 try {
-                    finalMyWriter.write(j.toString());
+                    finalMyWriter.write(j.toString()+"\n");
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             });
+            finalMyWriter.close();
             workList = objsS.get().flatMap(j -> {
                 JSONArray tempRef=null;
                 try{
